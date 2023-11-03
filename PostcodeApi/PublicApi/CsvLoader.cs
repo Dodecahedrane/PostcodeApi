0fc88ac8e2f161f301043cc03ac9eb4285bbe1b8
oliver.guy-1@students.plymouth.ac.uk <oliver.guy-1@students.plymouth.ac.uk>
2023-11-03 15:01:24 +0000

    DI for Loading CSV as Singleton

---
+++ PostcodeApi/PublicApi/CsvLoader.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+
+namespace PublicApi
+{
+    public class CsvLoader
+    {
+        private const string CsvFilePath = "C:\\Users\\Oliver\\Downloads\\ONSPD_AUG_2023_UK\\Data\\PostcodesLatLong.csv";
+
+        public List<PostcodeRecord> Records { get; private set; }
+
+        public CsvLoader() 
+        {
+            Records = GetRecords();
+        }
+
+        private List<PostcodeRecord> GetRecords()
+        {
+            using var reader = new StreamReader(CsvFilePath);
+            using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture));
+            return csv.GetRecords<PostcodeRecord>().ToList();
+        }
+    }
+
+    public interface ICsvLoader
+    {
+        List<PostcodeRecord> Records { get; set; }
+        List<PostcodeRecord> GetRecords();
+    }
+}
--- PostcodeApi/PublicApi/Program.cs
+++ PostcodeApi/PublicApi/Program.cs
@@ -1,13 +1,19 @@
+using PublicApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+//Add CsvLoader
+builder.Services.AddSingleton<CsvLoader>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
