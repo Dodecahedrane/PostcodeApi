7c825a16b005d7679f9b9b5d44cbc1967b3c0a1c
oliver.guy-1@students.plymouth.ac.uk <oliver.guy-1@students.plymouth.ac.uk>
2023-11-03 13:24:25 +0000

    CSV Record Class

---
+++ PostcodeApi/PublicApi/PostcodeRecord.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace PublicApi
+{
+    public class PostcodeRecord
+    {
+        [Name("pcd")]
+        public required string Postcode { get; set; }
+
+        [Name("lat")]
+        public required double Latitude { get; set; }
+
+        [Name("long")]
+        public required double Longitude { get; set; }
+    }
+}
