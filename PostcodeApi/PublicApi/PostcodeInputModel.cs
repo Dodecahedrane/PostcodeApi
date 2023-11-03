a5252497173cfe6e8abc3e7f556da2f47b1fc07a
oliver.guy-1@students.plymouth.ac.uk <oliver.guy-1@students.plymouth.ac.uk>
2023-11-03 15:37:50 +0000

    Postcode input model for body

---
+++ PostcodeApi/PublicApi/PostcodeInputModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PublicApi
+{
+    public class PostcodeInputModel
+    {
+        [Required]
+        [MaxLength(7)]
+        [MinLength(6)]
+        public required string Postcode { get; set; }
+    }
+}
