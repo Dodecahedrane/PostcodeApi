f1e195892cf67cd157d8a9ae6de3943a6099e3da
oliver.guy-1@students.plymouth.ac.uk <oliver.guy-1@students.plymouth.ac.uk>
2023-11-07 13:22:31 +0000

    Added a TODO

--- PostcodeApi/PublicApi/Program.cs
+++ PostcodeApi/PublicApi/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-//Add CsvLoader
+//Add Postcode Loader
+// TODO Get this from appsetting.json
 builder.Services.AddSingleton(new PostcodeLoader("wwwroot\\Data\\PostcodesLatLong.csv"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
