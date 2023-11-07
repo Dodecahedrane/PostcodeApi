3af34d01a5eff3197043c9b27bf1019ab2d90230
oliver.guy-1@students.plymouth.ac.uk <oliver.guy-1@students.plymouth.ac.uk>
2023-11-07 09:57:30 +0000

    Fix

--- PostcodeApi/PublicApi/PostcodeHelper.cs
+++ PostcodeApi/PublicApi/PostcodeHelper.cs
@@ -5,7 +5,7 @@ namespace PostcodeApi
 {
     public class PostcodeHelper
     {
-        string const pattern = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})";
+        public const string pattern = "^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\\s?[0-9][A-Za-z]{2})";
 
         public static bool IsPostcodeValid(string postcode)
         {
